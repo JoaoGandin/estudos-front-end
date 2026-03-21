eded5e6b3cfa4dd227240605265e1f827e8ea55e
JoaoGandin <joaovictorgoncalvesgandin@gmail.com>
2026-03-21 12:10:21 -0300

    add aula001 HTML and JavaScript files

---
+++ javascript/aula001/script.js
@@ -0,0 +1,10 @@
+alert("aasngvmksjangjk")
+document.getElementById("nome").value = "Joao"
+
+//comentario de uma linha.
+/*
+    Comentário de múltiplas linhas.
+    Esse tipo de comentário permite
+    que várias linhas sejam encapsuladas
+    em um mesmo comentário.
+*/
